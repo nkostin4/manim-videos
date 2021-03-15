765be332f0a3a25cb433d10bfcaf0fd7038e6568
nkostin <nkostin4@protonmail.com>
2021-03-15 15:27:45 -0600

    proof of concept

--- rotating_concentric_arcs.py
+++ rotating_concentric_arcs.py
@@ -1,25 +1,26 @@
 from manim import *
 
-class FirstRotating(Scene):
+class FirstRotating(ZoomedScene):
+    CONFIG = {
+                "zoomed_factor": 0.1
+            }
 
     def construct(self):
     
         arcs = VGroup()
     
-        radii = np.linspace(0.2, 3.8, 50)
+        radii = np.linspace(2, 100, 50)
         colors = [RED, BLUE]
         
-        for i in range(50):
+        for i in range(np.size(radii)):
             bin_alter = (len(colors) - i) % 2
             arc = Arc(radius = radii[i], angle = TAU/2, stoke_width=4, color=colors[bin_alter])
             arcs.add(arc)
             
         self.play(Write(arcs))
-        # self.play(GrowFromCenter(arcs))
         self.wait(1)
         
         # self.play(*[(Rotating(j, about_point = ORIGIN, radians = 26.315789*PI*j.get_arc_length(), rate_function = linear, run_time=200)) for j in arcs])
-        # self.play(*[(Rotating(j, about_point = ORIGIN, radians = (get_arc_length())/j, rate_function = linear, run_time=200)) for j in arcs])
-        self.play(*[(Rotating(j, about_point = ORIGIN, radians = 100*j.get_arc_length(), rate_function = linear, run_time=100)) for j in arcs])
+        self.play(*[(Rotating(j, about_point = ORIGIN, radians = 2*j.get_arc_length(), rate_function = linear, run_time=50)) for j in arcs])
         
         self.wait()
