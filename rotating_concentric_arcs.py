a17c44324ad82c3ada304cc3065197a9098e0471
nkostin <nkostin4@protonmail.com>
2021-03-15 14:43:38 -0600

    rotating arcs now working

--- rotating_concentric_arcs.py
+++ rotating_concentric_arcs.py
@@ -1,29 +1,24 @@
-#!/usr/bin/env python
-# Simon Pampena
+from manim import *
 
-from manimlib.imports import *
+class FirstRotating(Scene):
 
-class Dancing(Scene):
-    CONFIG = {
-        'num' : 50,
-        'angle' : TAU/2
-    }
-    
     def construct(self):
     
         arcs = VGroup()
     
-        radii = np.linspace(0.2, 3.8, self.num)
+        radii = np.linspace(0.2, 3.8, 50)
         colors = [RED, BLUE]
         
-        for i in range(self.num):
+        for i in range(50):
             bin_alter = (len(colors) - i) % 2
-            arc = Arc(radius = radii[i], angle = self.angle, stoke_width=4, color=colors[bin_alter])
+            arc = Arc(radius = radii[i], angle = TAU/2, stoke_width=4, color=colors[bin_alter])
             arcs.add(arc)
             
-        self.play(GrowFromCenter(arcs))
+        self.play(Write(arcs))
+        # self.play(GrowFromCenter(arcs))
         self.wait(1)
         
-        self.play(*[(Rotating(j, about_point = ORIGIN, radians = 26.315789*PI*j.get_arc_length(), rate_function = linear, run_time=200)) for j in arcs])
+        # self.play(*[(Rotating(j, about_point = ORIGIN, radians = 26.315789*PI*j.get_arc_length(), rate_function = linear, run_time=200)) for j in arcs])
+        # self.play(*[(Rotating(j, about_point = ORIGIN, radians = (get_arc_length())/j, rate_function = linear, run_time=200)) for j in arcs])
         
         self.wait()
