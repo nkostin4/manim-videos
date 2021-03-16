255b5fae97493d2b9e887b7f870f0718eeb91f40
nkostin <nkostin4@protonmail.com>
2021-03-16 03:16:23 -0600

    good progress

--- osculating_circles.py
+++ osculating_circles.py
@@ -28,3 +28,61 @@ class IntroText(Scene):
         self.play(FadeOut(top_text), FadeOut(middle_text), FadeOut(bottom_text))
         self.play(FadeOut(title))
         self.wait()
+
+class CoolPicture(Scene):
+
+    def construct(self):
+
+        path = ParametricFunction(lambda t: [2*np.cos(t)+np.cos(2*t), 2*np.sin(t)-np.sin(2*t), 0], t_min=0, t_max=TAU, stroke_width=10, color=BLUE)
+
+        self.add(path)
+
+        def get_radius(t):
+            return 4*np.sqrt(2)*np.sqrt(np.absolute(1-np.cos(3*t)))
+
+        def get_center(t):
+            ff = np.array([2*np.cos(t)+np.cos(2*t),-2*np.sin(t)-2*np.sin(2*t),-2*np.cos(t)-4*np.cos(2*t)])
+            gg = np.array([2*np.sin(t)-np.sin(2*t),2*np.cos(t)-2*np.cos(2*t),-2*np.sin(t)+4*np.sin(2*t)])
+            # x_center = ff[0] - ((ff[1]**2 + gg[1]**2)*gg[1])/(ff[1]*gg[2] - ff[2]*gg[1])
+            # y_center = gg[0] + ((ff[1]**2 + gg[1]**2)*ff[1])/(ff[1]*gg[2] - ff[2]*gg[1])
+            x_center = ((ff[1]**2 + gg[1]**2)*gg[1])/(ff[1]*gg[2] - ff[2]*gg[1])
+            y_center = ((ff[1]**2 + gg[1]**2)*ff[1])/(ff[1]*gg[2] - ff[2]*gg[1])
+            return [x_center, y_center]
+
+        t_vals = np.linspace(0.01, 2*np.pi/3, 25)
+        R = get_radius(t_vals)
+        Q = get_center(t_vals)
+        o_circles = VGroup()
+
+        for i in range(np.size(t_vals)):
+            circ = Circle(radius = R[i], color=GOLD).shift(Q[0][i], Q[1][i], 0)
+            o_circles.add(circ)
+
+        self.add(o_circles)
+
+class AlmostTriangle(Scene):
+
+    def construct(self):
+
+        path = ParametricFunction(lambda t: [2*np.cos(t)+np.cos(2*t), 2*np.sin(t)-np.sin(2*t), 0], t_min=0, t_max=TAU, stroke_width=10, color=BLUE)
+
+        moving_dot = Dot(radius=0.8, color=RED)
+        moving_dot.move_to(path.point_from_proportion(0))
+
+        t_offset = 0
+        rate = 0.25
+
+        def go_around(mob, dt):
+            t_offset += (dt*rate)
+            mob.move_to(path.point_from_proportion(self.t_offset % 1))
+
+        self.curve = VGroup()
+        self.curve.add(Line(self.curve_start,self.curve_start))
+
+        def get_curve():
+            last_line = self.curve[-1]
+            xkkkkkkkkkkkk
+
+        # self.add(curve)
+        # self.play(MoveAlongPath(moving_dot, curve), rate_func=linear, run_time=10)
+        self.wait()
