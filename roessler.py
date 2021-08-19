40b330ff2d92e106a4a791b1abceabe1bc82f189
nkostin <nkostin4@protonmail.com>
2021-08-19 14:50:47 -0600

    major brainlet

--- roessler.py
+++ roessler.py
@@ -1,6 +1,6 @@
 from manim import *
 from scipy.integrate import odeint
-config.background_color = "#0000FF"
+config.background_color = "#FF00BF"
 
 # Numerically solve ODE
 # Define constants
@@ -11,7 +11,7 @@ x_init_1 = 2.144246637147772105e+01; y_init_1 = -1.671644998751870559e-01; z_ini
 initial_state_1 = np.array([0.21*x_init_1 - 0.6, 0.21*y_init_1, 0.21*z_init_1 - 1.8])
 
 # Create array of time values to study
-t_init = 0; t_fin = 520; h = 0.02
+t_init = 0; t_fin = 20; h = 0.02
 t = np.arange(t_init, t_fin, h)
 
 # Governing system of differential equations
@@ -80,16 +80,16 @@ class RoesslerRotate(ThreeDScene):
         
         # self.add(axes)
         self.set_camera_orientation(phi = 2.5*PI/6, theta = 19*PI/12, distance = 4.6)
-        # self.play(Create(trajectory), run_time=5)
-        self.add(trajectory)
+        self.play(Create(trajectory), run_time=40)
+        # self.add(trajectory)
         self.wait(4)
-        # self.begin_ambient_camera_rotation(about = 'phi', rate = 2*PI/40)
-        # self.wait(20)
-        # self.stop_ambient_camera_rotation(about = 'phi')
-        # self.wait()
-        # self.play(FadeOut(trajectory), run_time = 2)
+        self.begin_ambient_camera_rotation(about = 'theta', rate = 2*PI/40)
+        self.wait(20)
+        self.stop_ambient_camera_rotation(about = 'theta')
+        self.wait()
+        self.play(FadeOut(trajectory), run_time = 2)
 
-class RoesslerDot(ThreeDScene):
+class RoesslerTrack(ThreeDScene):
 
     def construct(self):           
         
@@ -106,21 +106,27 @@ class RoesslerDot(ThreeDScene):
         trajectory = VMobject()
         trajectory.set_points_as_corners([*[coord(x,y,z) for x,y,z in self.tuples]])
 
-        path = VMobject(stroke_width=0.85)
-        path.set_color(WHITE)
-        dot = Dot()
+        length_tracker = ValueTracker(0)
+        x_tracker = ValueTracker(initial_state_1[0])
+        y_tracker = ValueTracker(initial_state_1[1])
+        z_tracker = ValueTracker(initial_state_1[2])
 
-        def update_path(path):
-            previous_path = path.copy()
-            previous_path.add_points_as_corners([dot.get_center()])
-            path.become(previous_path)
+        path = VMobject(stroke_width=2.85)
+        path.set_color(BLUE)
 
-        path.add_updater(update_path)
-        self.add(path, dot)
-        
+        def grow_roessler_path(path):
+            # x_tracker = trajectory.get_points()[0]
+            # y_tracker = trajectory.get_points()[1]
+            # z_tracker = trajectory.get_points()[2]
+            # path.add_points_as_corners(x_tracker.get_value(), y_tracker.get_value(), z_tracker.get_value())
+            # path.set_points_smoothly([*[coord(x,y,z) for x,y,z in self.tuples]])
+            path.add_points_as_corners([trajectory.get_last_point()])
+            # path.set_points_smoothly(trajectory.get_points())
+            length_tracker = path.get_arc_length()
+
+        path.add_updater(grow_roessler_path)
+
         self.set_camera_orientation(phi = 2.5*PI/6, theta = 19*PI/12, distance = 4.6)
-
-        self.play(
-                MoveAlongPath(dot, trajectory, rate_func=rate_functions.ease_in_sine), run_time=10
-            )
-        self.wait(4)
+        self.add(path)
+        self.play(length_tracker.animate.increment_value(trajectory.get_arc_length()), run_time=10)
+        self.wait(1)
